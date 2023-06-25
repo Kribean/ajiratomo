b7a6023476eac84f4313fcf3dd165b2b65340637
Ramael <ramael.bruno@gmail.com>
2023-06-26 01:11:01 +0200

    one

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
-    basePath:'/ajiratomo',
-    assetPrefix:'/ajiratomo'
+
+  basePath:'/ajiratomo',
+
 
   }
    
